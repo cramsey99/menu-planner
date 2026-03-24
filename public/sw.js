ec8979c225f4b13c50bcf102e009871d78f861dc
Clint Ramsey <clint.ramsey@aveva.com>
2026-03-24 14:33:16 -0500

    make pwa app

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'menu-planner-v1';
+const PRECACHE = [
+    '/',
+    '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    // Network first, fall back to cache
+    if (e.request.method !== 'GET') return;
+    e.respondWith(
+        fetch(e.request)
+            .then(response => {
+                const clone = response.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+                return response;
+            })
+            .catch(() => caches.match(e.request))
+    );
+});
